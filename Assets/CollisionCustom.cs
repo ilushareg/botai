4ed34abfdd65a2f30ef1a582f2bc0e18af8d25cf
ilushareg <ilushareg@gmail.com>
2018-09-30 00:52:24 +0300

    Collision test harness and Unity RayTrace are working

---
+++ Assets/CollisionCustom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCustom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BoxCollider[] c = transform.GetComponentsInChildren<BoxCollider>();
+        foreach(BoxCollider b in c)
+        {
+
+            Debug.Log(b.name);
+            Debug.Log(b.transform);
+            Debug.Log(b.center);
+            Debug.Log(b.size);
+            //Generate planes for that cube
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TraceTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        GameObject target = GameObject.Find("Sphere_target");
+        Vector3 start = transform.position;
+        Vector3 end = target.transform.position;
+        Vector3 dir = end - start;
+
+        RaycastHit[] hit = Physics.RaycastAll(start + dir.normalized, dir.normalized, dir.magnitude-1.0f, 0xffff);
+
+        foreach (RaycastHit hitInfo in hit)
+        {
+
+            Debug.DrawLine(hitInfo.point, hitInfo.point + Vector3.up * 10);
+        }
+
+        Debug.DrawLine(start, end);
+
+    }
+}
